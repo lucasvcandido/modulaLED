f9b5ac99a4b26f2b2bc3a7ce78b9d9d7e243b95f
19180064 <19180064@DSN1271137815.cfp127aluno.local>
2020-03-13 08:40:53 -0300

    Dois LEDs alternando em 0.5s. Sem condição de acionamento.

---
+++ modulaLED.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=modulaLED.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/modulaLED.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=modulaled.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/modulaled.x.tar
